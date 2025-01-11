8a5290ea0dc302c87bb2088afe25d7e0be50920c
FranckHeCai <senlouboi.egame@gmail.com>
2025-01-11 18:13:28 +0100

    conference ticket css part1 done

--- conference-ticket/src/components/FormComponent.jsx
+++ conference-ticket/src/components/FormComponent.jsx
@@ -1,20 +1,94 @@
 import { useFormContext } from "../context/FormContext.jsx"
 import "./Form.css"
+import uploadIcon from "../assets/images/icon-upload.svg"
+import infoIcon from "../assets/images/icon-info.svg"
+import { useState } from "react"
 
 export const FormComponent = () =>{
     
     const {handleForm} = useFormContext()
+    const [dragActive, setDragActive] = useState(false)
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
 
     const onSubmit = (event) => {
         event.preventDefault()
         handleForm(event.target)
       }
+
+      const handleDrag = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (event.type === "dragenter" || event.type === "dragover") {
+          setDragActive(true);
+        } else if (event.type === "dragleave") {
+          setDragActive(false);
+        }
+      };
+    
+      const handleDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setDragActive(false);
+        if (event.dataTransfer.files && event.dataTransfer.files[0]) {
+          const fileInput = document.getElementById("file-input");
+          fileInput.files = event.dataTransfer.files;
+          setPreview(URL.createObjectURL(fileInput.files[0]));
+      };
+    }
+
+      const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setFile(file);
+        setPreview(URL.createObjectURL(file));
+      };
+    
+      const handleRemoveFile = () => {
+        setFile(null);
+        setPreview(null);
+        document.getElementById("file-input").value = "";
+      };
   
     return(
       <form onSubmit={onSubmit} className="ticket-form">
-          <label htmlFor="file-input">Upload Avatar
+          <label htmlFor="file-input">Upload Avatar</label>
+          <label 
+            htmlFor="file-input" 
+            className={`input-area ${dragActive? "drag-active:" : ""}`}
+            onDragEnter={handleDrag}
+            onDragOver={handleDrag}
+            onDragLeave={handleDrag}
+            onDrop={handleDrop}
+          >     
+          
+          <input
+          hidden
+          required
+          id="file-input"
+          type="file"
+          accept="image/*"
+          name="file-input"
+          onChange={handleFileChange}
+          />
+          {preview ? (
+            <div className="preview">
+              <img src={preview} alt="Preview" className="preview-image" />
+              <button type="button" onClick={handleRemoveFile}>
+                Remove
+              </button>
+            </div>
+          ) : (
+            <>
+              <img className="input-icon" src={uploadIcon} alt="Upload Icon" />
+              <p>Drag and drop or click to upload</p>
+            </>
+          )}      
+                
           </label>
-          <input required id="file-input" type="file" accept="image/*" name="file-input"/>
+          <div className="info">
+            <img src={infoIcon} alt={infoIcon} />
+            <p>Upload your photo (JPG or PNG, max size 500KB).</p>
+          </div>
           <label htmlFor="full-name">Full Name</label>
           <input id="full-name" type="text" name="full-name" required />
           <label htmlFor="email">Email Address</label>
--- conference-ticket/src/components/Header.jsx
+++ conference-ticket/src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
         <header>
                 <img src={logoMark} alt="ticket pattern" />
                 <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
-                <p>Secure your spot at next year's biggest coding conference</p>
+                <p>Secure your spot at next year's biggest coding conference.</p>
         </header>
     )
 }
