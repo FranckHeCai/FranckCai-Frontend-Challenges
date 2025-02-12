e829437aaa43a594f1bfda6d36fbc43507c2dd7c
Franck Cai <senlouboi.egame@gmail.com>
2025-02-12 19:26:45 +0100

    mortgage last changes done

--- mortgage-repayment-calculator-main/script.js
+++ mortgage-repayment-calculator-main/script.js
@@ -25,14 +25,14 @@ const updateUI = (type) =>{
    <h3 class="text-xl font-medium text-white mb-1">Your results</h3>
         <p class="text-white/50 mb-4">Your results are shown below based on the information you provided.
         To adjust the results, edit the form and click “calculate repayments” again.</p>
-        <div class="p-6 bg-[var(--slate-dark)] rounded-lg border-t-3 border-[var(--lime)]">
+        <div class="p-6 bg-[var(--slate-dark)] rounded-3xl border-t-3 border-[var(--lime)]">
           <div>
             <p class="text-white/50 mb-1">Your monthly repayments</p>
-            <p class="text-5xl text-[var(--lime)] font-bold">€${monthlyPayment}</p>
+            <p class="text-5xl text-[var(--lime)] font-bold">€${monthlyPayment.toFixed(2)}</p>
           </div>
           <div class="mt-6 pt-6 border-t-3 border-white/20">
             <p class="text-white/50">Total you'll repay over the term</p>
-            <p class="text-white text-xl font-bold">€${totalPayment}</p>
+            <p class="text-white text-xl font-bold">€${totalPayment.toFixed(2)}</p>
           </div>
         </div>
   `
