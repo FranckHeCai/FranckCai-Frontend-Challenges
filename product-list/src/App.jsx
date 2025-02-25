b5c539c1f73317fd57ccdd66cabba8827bdc8faf
FranckHeCai <senlouboi.egame@gmail.com>
2025-02-25 22:44:57 +0100

    product list done

--- product-list/src/App.jsx
+++ product-list/src/App.jsx
@@ -7,12 +7,12 @@ const INITIAL_STATE = {
   }
 
 function App() {
-  const [cart, setCart] = useState([])
   const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE)
   
   function cartReducer(state, action){
     switch (action.type) {
       case "ADD":
+      console.log(state.cart)
       let newCart
       const foundProduct = data.find(product => product.name === action.payload)
       const inCart = state.cart.find(item => item.name === foundProduct.name)
@@ -25,8 +25,9 @@ function App() {
         }else{
           newCart = [...state.cart, {...foundProduct, quantity: 1}]
         } 
-        const newPrice = state.cart.reduce((total, item) => total + item.quantity * item.price,0)
-        return {cart: newCart, price : newPrice}
+        const newPrice = newCart.reduce((total, item) => total + item.quantity * item.price,0)
+        console.log(newCart, newPrice)
+        return {cart: newCart, price: newPrice}
 
       case "CLEAR":
         return INITIAL_STATE
@@ -64,17 +65,17 @@ function App() {
   }
 
   return (
-    <>
-      <main className='p-10 flex flex-col sm:flex-row gap-3 justify-between items-start'>
+    <main className='p-10'>
+      <h2 className='text-4xl mb-7'>Desserts</h2>
+      <div className='flex flex-col sm:flex-row gap-3 justify-between items-start'>
         <div className='products flex flex-col'>
-          <h2 className='text-4xl mb-7'>Desserts</h2>
           <div className='products grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
           {data.map(product => {
                 return(
-                  <div className='rounded-lg'>
+                  <div key={product.name} className='rounded-lg'>
                     <div className='relative'>
                       <img className='w-full rounded-lg mb-7' src={product.image.desktop} alt={product.name} />
-                      <button onClick={()=>{handleAddCart(product.name)}} className='font-medium py-2 px-5 bg-[var(--rose50)] rounded-full absolute top-43 left-9.5 active:bg-[var(--red)] active:text-white'>Add to Cart</button>
+                      <button onClick={()=>{handleAddCart(product.name)}} className='font-medium py-2 px-5 bg-[var(--rose50)] rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-[-50%] w-full sm:max-w-40 active:bg-[var(--red)] active:text-white'>Add to Cart</button>
                     </div>
                     <p className='text-black/50'>{product.name}</p>
                     <p className='font-bold'>{product.category}</p>
@@ -85,19 +86,19 @@ function App() {
               }
           </div>
         </div>
-        <div className='sm:w-2xl cart p-5 bg-[var(--rose50)] rounded-lg flex flex-col gap-5'>
+        <div className={`sm:w-2xl cart p-5 bg-[var(--rose50)] ${state.cart.length !==0 ? 'border-2 border-[var(--red)]' : ''} rounded-lg flex flex-col gap-5 `}>
           <div className="flex justify-between items-center">
             <h3 className='text-[var(--red)] font-bold'>Your Cart</h3>
-            { cart.length>0 &&
+            { state.cart.length>0 &&
               <button onClick={()=>{clearCart()}} className='py-1 px-3 bg-[var(--red)] rounded-full text-[var(--rose50)] text-sm'>Clear</button>
             }
           </div>
           <div className="flex flex-col justify-center gap-3">
-            {cart.length!==0
+            {state.cart.length!==0
               ? (
                   <>
-                    {cart.map(item => {
-                      return <div className='flex justify-between'>
+                    {state.cart.map(item => {
+                      return <div key={item.name} className='flex justify-between'>
                         <p className='text-sm font-bold'>{item.name}</p>
                         <p>{item.quantity}x {item.price}€</p>
                       </div>
@@ -117,17 +118,17 @@ function App() {
             <img src="/icon-carbon-neutral.svg" alt="" />
             <p className='text-sm'>this is a <span className='font-bold'>carbon-neutral</span> delivery</p>
           </div>
-          { cart.length !== 0 &&
+          { state.cart.length !== 0 &&
             <button className='py-3 px-5 bg-[var(--red)] rounded-full text-[var(--rose50)]'>Confirm Order</button>
           }
         </div>
         
-      </main>
+      </div>
       <div className="attribution text-center">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
       Coded by <a href="https://github.com/FranckHeCai" target='_blank'>Franck Cai</a>.
       </div>
-    </>
+    </main>
     
   )
 }
